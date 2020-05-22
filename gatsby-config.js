632d74c47d465625ae87d61e6baab09465d3a274
DMorton <dmorton@ie.ibm.com>
2020-05-22 18:17:39 +0100

    blog updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,8 +86,8 @@ module.exports = {
                 return Object.assign({}, edge.node.frontmatter, {
                   title: edge.node.frontmatter.title,
                   description: edge.node.excerpt,
-                  url: `${site.siteMetadata.siteUrl}blog/${edge.node.fields.slug}`,
-                  guid: `${site.siteMetadata.siteUrl}blog/${edge.node.fields.slug}`,
+                  url: `${site.siteMetadata.siteUrl}/blog${edge.node.fields.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}/blog${edge.node.fields.slug}`,
                   custom_elements: [{ 'content:encoded': edge.node.html }],
                   keywords: edge.node.frontmatter.keywords.split(','),
                 })
