099947e5fce706c84002d6d7c0884ae8bf592b62
David Morton <davidmortonmail@gmail.com>
2021-04-04 16:56:58 +0100

    blog updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 require('./src/styles/prism-material-dark.css')
 require('./src/styles/global.css')
 
-exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
+exports.onServiceWorkerUpdateReady = () => window.location.reload();
